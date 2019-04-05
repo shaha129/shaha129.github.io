e3bda0aea56de6c71d9496f8be4419b63575609f
SHAHA129 <shaha129@newschool.edu>
2019-04-04 20:02:26 -0400

    AMA

---
+++ AMA/main1.js
@@ -0,0 +1,346 @@
+console.log('AMA');
+
+$(document).ready(function(){
+  $("img").mouseover(function(){
+    $(this).closest('.image-set').find('.text').fadeIn();
+  });
+
+  $("img").mouseout(function(){
+    $(this).closest('.image-set').find('.text').fadeOut();
+  });
+});
+
+
+// $(document).ready(function(){
+//   $("#teriyaki").mouseover(function(){
+//     $("#text1").fadeIn();
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#teriyaki").mouseout(function(){
+//     $("#text1").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#march23").mouseover(function(){
+//     $("#text2").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#march23").mouseout(function(){
+//     $("#text2").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#cinnamon").mouseover(function(){
+//     $("#text3").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#cinnamon").mouseout(function(){
+//     $("#text3").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#gooddoc").mouseover(function(){
+//     $("#text4").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#gooddoc").mouseout(function(){
+//     $("#text4").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#loreal").mouseover(function(){
+//     $("#text3").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#loreal").mouseout(function(){
+//     $("#text3").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#missile").mouseover(function(){
+//     $("#text5").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#missile").mouseout(function(){
+//     $("#text5").fadeOut();
+//   });
+// });
+
+
+// $(document).ready(function(){
+//   $("#badknees").mouseover(function(){
+//     $("#text2").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#badknees").mouseout(function(){
+//     $("#text2").fadeOut();
+//   });
+// });
+
+
+// $(document).ready(function(){
+//   $("#bealright").mouseover(function(){
+//     $("#text6").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#bealright").mouseout(function(){
+//     $("#text6").fadeOut();
+//   });
+// });
+
+
+
+
+// $(document).ready(function(){
+//   $("#aladdin").mouseover(function(){
+//     $("#text7").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#aladdin").mouseout(function(){
+//     $("#text7").fadeOut();
+//   });
+// });
+
+
+
+
+// $(document).ready(function(){
+//   $("#calculus").mouseover(function(){
+//     $("#text8").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#calculus").mouseout(function(){
+//     $("#text8").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#bee").mouseover(function(){
+//     $("#text9").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#bee").mouseout(function(){
+//     $("#text9").fadeOut();
+//   });
+// });
+
+
+// $(document).ready(function(){
+//   $("#batman").mouseover(function(){
+//     $("#text10").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#batman").mouseout(function(){
+//     $("#text10").fadeOut();
+//   });
+// });
+
+
+// $(document).ready(function(){
+//   $("#atypical").mouseover(function(){
+//     $("#text11").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#atypical").mouseout(function(){
+//     $("#text11").fadeOut();
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#a4dimensions").mouseover(function(){
+//     $("#text7").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#a4dimensions").mouseout(function(){
+//     $("#text7").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#aurat").mouseover(function(){
+//     $("#text12").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#aurat").mouseout(function(){
+//     $("#text12").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#bhutto").mouseover(function(){
+//     $("#text13").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#bhutto").mouseout(function(){
+//     $("#text13").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#nineteen75").mouseover(function(){
+//     $("#text14").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#nineteen75").mouseout(function(){
+//     $("#text14").fadeOut();
+//   });
+// });
+
+
+// $(document).ready(function(){
+//   $("#aangan").mouseover(function(){
+//     $("#text9").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#aangan").mouseout(function(){
+//     $("#text9").fadeOut();
+//   });
+// });
+
+
+// $(document).ready(function(){
+//   $("#airmax").mouseover(function(){
+//     $("#text15").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#airmax").mouseout(function(){
+//     $("#text15").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#sajni").mouseover(function(){
+//     $("#text16").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#sajni").mouseout(function(){
+//     $("#text16").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#aliexpress").mouseover(function(){
+//     $("#text17").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#aliexpress").mouseout(function(){
+//     $("#text17").fadeOut();
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#appropriation").mouseover(function(){
+//     $("#text18").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#appropriation").mouseout(function(){
+//     $("#text18").fadeOut();
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#colorcode").mouseover(function(){
+//     $("#text19").fadeIn();
+   
+//   });
+// });
+
+// $(document).ready(function(){
+//   $("#colorcode").mouseout(function(){
+//     $("#text19").fadeOut();
+//   });
+// });
